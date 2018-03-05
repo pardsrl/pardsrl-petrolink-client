2590a3da0ea0d4adf58bc263b165eaa5cb249227
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2018-03-05 19:12:17 -0300

    Remove comment code. Example data

--- server.js
+++ server.js
@@ -8,26 +8,9 @@ const server = net.createServer((c) => {
   c.write('hello\r\n')
   c.pipe(c)
 
-  c.on('data', (buffer) => console.log(buffer.toString('ascii')))
+  c.on('data', (buffer) => console.log(buffer))
 })
-// server.on('error', (err) => {
-//   throw err;
-// });
+
 server.listen(9000, '0.0.0.0', () => {
   console.log('server bound')
 })
-
-// var client = new net.Socket();
-// client.connect(9000, '127.0.0.1', function() {
-// 	console.log('Connected');
-// 	client.write('Hello, server! Love, Client.');
-// });
-
-// client.on('data', function(data) {
-// 	console.log('Received: ' + data);
-// 	client.destroy(); // kill client after server's response
-// });
-
-// client.on('close', function() {
-// 	console.log('Connection closed');
-// });
