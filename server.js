3adb4f39db4b784e0c15cc855bf63e26b22f7abe
Santiago Semhan <santiagosemhan@gmail.com>
2018-03-06 08:51:43 -0300

    Fix server bugs.

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const server = net.createServer((c) => {
   c.write('hello\r\n')
   c.pipe(c)
 
-  c.on('data', (buffer) => console.log(buffer))
+  c.on('data',  (buffer) => console.log(buffer.toString('ascii')))
+
+  c.on('error', (err) => console.log(err.message))
 })
+
 
 server.listen(9000, '0.0.0.0', () => {
   console.log('server bound')
